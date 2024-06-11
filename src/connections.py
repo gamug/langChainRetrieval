d8b2c3a57428aed7d387c86856e8b39e2a6cbee5
Gabriel Jaime Múnera González <32693913+gamug@users.noreply.github.com>
2024-06-11 10:08:01 -0500

    Update connections.py

--- src/connections.py
+++ src/connections.py
@@ -1,11 +1,11 @@
 import os
 
 def set_connections():
-    os.environ["OPENAI_API_TYPE"] = 'api_key'
-    os.environ["OPENAI_API_KEY"] = '6286ae4863e54163bc669839b2531f1a'
-    os.environ["OPENAI_EMBED_API_KEY"] = 'bb3d1d96f030475e918408f131285649'
-    os.environ["AZURE_ENDPOINT1"] = 'https://oai-llm-poc-ecp-sbx.openai.azure.com/'
-    os.environ["AZURE_ENDPOINT2"] = 'https://oai-llmca-poc-ecp-sbx.openai.azure.com/'
-    os.environ["AZURE_GPT_DEPLOY"] = 'Anie'
-    os.environ["AZURE_EMBED_DEPLOY"] = 'genia-embedding'
+    os.environ["OPENAI_API_TYPE"] = '***'
+    os.environ["OPENAI_API_KEY"] = '******'
+    os.environ["OPENAI_EMBED_API_KEY"] = '******'
+    os.environ["AZURE_ENDPOINT1"] = '********'
+    os.environ["AZURE_ENDPOINT2"] = '******'
+    os.environ["AZURE_GPT_DEPLOY"] = '*******'
+    os.environ["AZURE_EMBED_DEPLOY"] = '******'
     os.environ["OPENAI_API_VERSION"] = '2023-12-01-preview'
